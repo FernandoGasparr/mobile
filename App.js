4f12401d8b2f35da6813a936cbafa409c8dd7b12
Abimael-Lovera <abimaellovera@gmail.com>
2022-03-20 19:37:26 -0300

    Add Header

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header/Header';
 import Navigation from './Navigation';
 
 export default function App() {
-	return <Navigation />;
+	return (
+		<>
+			<Header />
+			<Navigation />
+		</>
+	);
 }
 
 const styles = StyleSheet.create({
---
+++ components/Header/Header.js
@@ -0,0 +1,18 @@
+import { View, Text, Image } from 'react-native';
+import logo from '../assets/icon.png';
+
+const Header = () => {
+	return (
+		<View>
+			<Image
+				source={logo}
+				style={{
+					width: 100,
+					height: 50,
+				}}
+			/>
+		</View>
+	);
+};
+
+export default Header;
