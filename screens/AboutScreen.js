042342798603f1a0332565de86ab4400dd4791c2
Abimael-Lovera <abimaellovera@gmail.com>
2022-03-20 17:57:12 -0300

    Create Screens Home About Rights

---
+++ screens/AboutScreen.js
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const AboutScreen = () => {
+	return (
+		<View>
+			<Text
+				style={{
+					fontSize: 30,
+				}}
+			>
+				AboutScreen Sobre Nós
+			</Text>
+		</View>
+	);
+};
+
+export default AboutScreen;
---
+++ screens/RightsScreen.js
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const RightsScreen = () => {
+	return (
+		<View>
+			<Text
+				style={{
+					fontSize: 30,
+				}}
+			>
+				RightsScreen seus direitos
+			</Text>
+		</View>
+	);
+};
+
+export default RightsScreen;
