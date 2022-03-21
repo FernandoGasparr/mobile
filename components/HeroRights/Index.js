62ec4f8ee3d16d5869deaac8a432afb5cbae97ad
Abimael-Lovera <abimaellovera@gmail.com>
2022-03-21 16:20:43 -0300

    Create and insert component heroRights in RightsScreen

--- components/HeroRights/Index.js
+++ components/HeroRights/Index.js
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { theme } from '../../styles/theme';
+import mulher from '../../assets/mulher.png';
 
 const HeroRights = () => {
 	return (
-		<View>
-			<Text>HeroRights</Text>
+		<View style={styles.container}>
+			<View style={styles.content}>
+				<Text style={styles.title}>Seus</Text>
+				<Text style={styles.title}>Diretos</Text>
+			</View>
+			<Image source={mulher} style={styles.img} />
 		</View>
 	);
 };
 
+const styles = StyleSheet.create({
+	container: {
+		width: '100%',
+		backgroundColor: theme.colors.skin,
+		flexDirection: 'row',
+		justifyContent: 'center',
+	},
+	content: {
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	title: {
+		textAlign: 'center',
+		color: theme.colors.beige,
+		fontSize: 28,
+		fontFamily: theme.fonts.bold,
+	},
+
+	img: {
+		width: 200,
+		height: 200,
+		resizeMode: 'contain',
+	},
+});
 export default HeroRights;
