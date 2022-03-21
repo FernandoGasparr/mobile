a6e095cb6ef580e4421383f335c6959eabc4957d
Abimael-Lovera <abimaellovera@gmail.com>
2022-03-21 14:24:51 -0300

    Create and insert component Solucao in AboutScreen

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,12 +1,14 @@
 import { View, Text, StyleSheet, SafeAreaView, ScrollView } from 'react-native';
 import { theme } from '../styles/theme';
 import Team from '../components/Team';
+import Solucao from '../components/solucao/Index';
 
 const AboutScreen = () => {
 	return (
 		<View style={styles.container}>
 			<SafeAreaView style={styles.container}>
 				<ScrollView style={styles.scrollView}>
+					<Solucao />
 					<Team />
 				</ScrollView>
 			</SafeAreaView>
