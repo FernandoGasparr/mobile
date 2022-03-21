6729df7966f0f6691e420637e3f43685a6aa0d02
Abimael-Lovera <abimaellovera@gmail.com>
2022-03-21 00:49:05 -0300

    Team add url  github linkedin email

--- components/Team/index.js
+++ components/Team/index.js
@@ -10,40 +10,64 @@ import rodrigo from '../../assets/rodrigo.png';
 const Team = () => {
 	const members = [
 		{
+			id: 1,
 			nome: 'Abimael Lovera',
 			img: abimael,
 			dev: 'Desenvolverdor FullStack',
+			github: '',
+			linkedin: '',
+			email: '',
 		},
 		{
+			id: 2,
 			nome: 'Fernando Gaspar',
 			img: fernando,
 			dev: 'Desenvolverdor FullStack',
+			github: '',
+			linkedin: '',
+			email: '',
 		},
 		{
+			id: 3,
 			nome: 'Gustavo Silva',
 			img: gustavo,
 			dev: 'Desenvolverdor FullStack',
+			github: '',
+			linkedin: '',
+			email: '',
 		},
 		{
+			id: 4,
 			nome: 'Rodrigo Cunha',
 			img: rodrigo,
 			dev: 'Desenvolverdor FullStack',
+			github: '',
+			linkedin: '',
+			email: '',
 		},
 		{
+			id: 5,
 			nome: 'Jonatan Vasconcelos',
 			img: jonatan,
 			dev: 'Desenvolverdor FullStack',
+			github: '',
+			linkedin: '',
+			email: '',
 		},
 		{
+			id: 6,
 			nome: 'Renan dos Reis Fernandes',
 			img: renan,
 			dev: 'Desenvolverdor FullStack',
+			github: '',
+			linkedin: '',
+			email: '',
 		},
 	];
 	return (
 		<View style={styles.container}>
 			{members.map(member => {
-				return <Member nome={member.nome} img={member.img} />;
+				return <Member nome={member.nome} img={member.img} key={member.id} />;
 			})}
 		</View>
 	);
